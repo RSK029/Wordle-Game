4896b2582a2e32c47d5ccce7cba9cbf92fd67c3f
Rohan <rohansaini029@gmail.com>
2026-06-25 10:25:57 +0530

    word validation added

--- script.js
+++ script.js
@@ -8,13 +8,11 @@ let currentAllowedIndex = 0;
 
 
 letterInputs.forEach((input, index) => {
-    input.addEventListener("keyup", function(event) {
+    input.addEventListener("keyup", async function(event) {
 
         if (event.key === "Backspace") {
-            if(currentAllowedIndex > 0) { 
-                console.log(currentAllowedIndex);
+            if(currentAllowedIndex > 0) {                 
                 currentAllowedIndex--;
-                console.log(currentAllowedIndex);
                 board[currentRow].pop();
                 letterInputs[currentAllowedIndex].value = "";
                 letterInputs[currentAllowedIndex].focus();
@@ -25,13 +23,18 @@ letterInputs.forEach((input, index) => {
 
         if (event.key === "Enter") {
             if (board[currentRow].length === 5) {
-                checkWord();
-
-                currentRow++;  
-                let nextRow = letterInputs[currentRow * 5];  
-                if (nextRow) {
-                    nextRow.focus();
+                const currentGuess = board[currentRow].join("");
+                const isValid = await validateWord(currentGuess);
+                
+                if(isValid){
+                    currentRow++;  
+                    let nextRow = letterInputs[currentRow * 5];  
+                    if (nextRow) {
+                        nextRow.focus();
                 }
+                }
+
+                
             }
             return;
         }
@@ -107,35 +110,68 @@ async function init() {
 init();
 
 
-function checkWord() {
+async function checkWord() {
     
     const currentGuess = board[currentRow].join("");
     console.log("Word of the day:", wordOfTheDay);
     wordArray = wordOfTheDay.split("");
-    console.log("wordArray before marking letters as used:", wordArray);
+    
     for (let i = 0; i < currentGuess.length; i++) {
-        
+        console.log("entered checkWord loop");
         const guess = currentGuess[i];
         const correct = wordOfTheDay[i]; 
         const word = "word" + `${currentRow}`;
         const letter = "l" + `${i}`;
+        console.log(`.${word} .${letter}`);
         const input = document.querySelector(`.${word} .${letter}`);
 
         if (guess === correct && wordArray.includes(guess)) {  
             wordArray[i] = null; // Mark the letter as used
             input.style.backgroundColor = "green";   
-            console.log(`wordArray after marking ${guess} as used:`, wordArray); 
+            
         }
 
         else if (wordArray.includes(guess)) {
             wordArray[wordArray.indexOf(guess)] = null; // Mark the letter as used
             input.style.backgroundColor = "yellow";    
-            console.log(`wordArray after marking ${guess} as used:`, wordArray);
+            
         }
         else if (!wordArray.includes(guess)) {
             wordArray[wordArray.indexOf(guess)] = null; // Mark the letter as used
             input.style.backgroundColor = "gray";    
-            console.log(`wordArray after marking ${guess} as used:`, wordArray);
+            
         }
     }       
 }
+
+let isValidWord = false;
+
+async function validateWord(word) {
+    try{
+        const response = await fetch("https://words.dev-apis.com/validate-word", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ "word": `${word}` })
+        });
+        console.log(response.status);
+        console.log(response.ok);
+
+        const data = await response.json();
+        console.log("Validation response:", data);
+        isValidWord = data.validWord;
+        
+        if(isValidWord) {
+                    
+                    await checkWord();
+                }
+        return isValidWord;
+    }
+    catch (error) {
+        console.error("Error validating word:", error);
+        return false;
+    }
+}
+
+
